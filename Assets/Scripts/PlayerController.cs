585fb0b903089c5ce8b3b38eb4e79b28ae8400ac
Patar595 <pthayer595@yahoo.com>
2021-11-20 10:38:02 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    private float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate((Vector3.left * Time.deltaTime * speed));
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate((Vector3.down * Time.deltaTime * speed * 2));
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate((Vector3.up * Time.deltaTime * speed * 2));
+        }
+        
+    }   
+        
+        
+
+}
